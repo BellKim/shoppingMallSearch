e5507cbbf18c6b3206bd1e26f326b9784944114c
JongHyun <goste390@gmail.com>
2020-04-12 21:17:24 +0900

    push routerLink

--- vue_trello/lecture-vue-trello/src/App.vue
+++ vue_trello/lecture-vue-trello/src/App.vue
@@ -1,11 +1,19 @@
 <template>
   <div id="app">
-      <h2> hello world tjqnxj여기서부터 코드를 시작합니다.</h2>
 
       <!-- 해더 -->
 
       <!-- 본문 -->
-      <router-view></router-view> <!-- 라우터에의해서 대체되는 영역이다 -->
+      <h2> hello world tjqnxj여기서부터 코드를 시작합니다.</h2>
+
+
+
+      <router-link to="/"> home</router-link>
+      <router-link to="/login"> login</router-link>
+
+
+
+      <router-view></router-view> <!-- 라우터에의해서 대체되는 영역이다 루트컴포넌트.  -->
 
       <!-- 푸터 -->
 
