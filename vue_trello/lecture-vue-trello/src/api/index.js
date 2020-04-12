1aa802e3199612074760e7dd987c8478c9665c47
JongHyun <goste390@gmail.com>
2020-04-13 05:29:33 +0900

    login 처리

--- vue_trello/lecture-vue-trello/src/api/index.js
+++ vue_trello/lecture-vue-trello/src/api/index.js
@@ -8,7 +8,7 @@ import router from '../router'
 const DOMAIN = 'http://localhost:3000'
 const UNAUTHORIZED = 401
 const onUnauthorized = () => {
-    router.push('./login')
+    router.push(`/login?rPath=${encodeURIComponent(location.pathname)}`)
 }
 
 const request = (method, url, data) => {
@@ -16,17 +16,35 @@ const request = (method, url, data) => {
         method,
         url:DOMAIN + url,
         data
-    }).then(result => result.data)
+    }).then(result => result.data) 
     .catch(result => {
         const {status} = result.response
-        if(status === UNAUTHORIZED) return onUnauthorized()
-        throw Error(result)
+        if(status === UNAUTHORIZED) onUnauthorized()
+        throw result.response
     })
 }
+
+export const setAuthInHeader = token =>{ //토큰값을 router 로 전달해주는 역할을 한다.
+    // 토큰정보를 받아서 엑시오스 라이브리 안에다 추가할건데
+    // 모든리퀘스트를 날리기 전에 해더값을 토큰정보로 설정하는 역할을 한다. 
+
+    axios.defaults.headers.common['Authorization'] = token ? `Bearer ${token}` : null;
+
+}
+
+const {token} = localStorage
+if (token) setAuthInHeader(token)
+
 
 export const board = {
     fetch(){
         return request('get', '/boards')
+    }
+}
+
+export const auth = {
+    login(email, password){
+        return request('post', '/login', {email, password})
     }
 }
 
